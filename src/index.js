cb658be83eb3f6e3addcfa22e6a3da2d8e2f961d
The Man <msblauwal6@gmail.com>
2023-09-13 15:03:29 +0200

    Change port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { google } = require("googleapis");
 const bodyParser = require('body-parser');
 
 const app = express()
-const port = 3000
+const port = 8080
 
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
